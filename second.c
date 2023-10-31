bd5a7b708534c86b5cc236bce30ef43c125eed3c
prana36 <pranavarmarkar36@gmail.com>
2023-10-31 22:27:19 +0530

    modified

---
+++ second.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+//#include<conio.h>
+int main() 
+{
+    int Num,a;
+    printf("Enter a No. = ");
+    scanf("%d",&Num);
+    a=Num/2;
+   // printf("%d",a);
+       {
+             if(a==0)
+             printf("\nEven No. ");
+         
+         else
+         printf("\nOdd No. ");
+        }
+   return 0;
+}
